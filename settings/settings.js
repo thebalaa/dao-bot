c7cf1d7742eb62dec79bac98f90f10e079f69409
Peter C <PeterChauYEG@users.noreply.github.com>
2016-04-28 10:18:34 -0600

    Create settings.js

---
+++ settings/settings.js
@@ -0,0 +1,17 @@
+module.exports = {
+    bot: {
+        guide: ''
+    },
+    slack: {
+        token: ''
+    },
+    daoApi: {
+        host: '',
+        port: 8080,
+    },
+    contract: {
+        name: '',
+        address: '',
+        abi: ''
+    },
+}
